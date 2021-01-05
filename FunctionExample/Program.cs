2183eb861b9b9b986bd95510d33195616efec6c2
Serkan <serkanerengit@outlook.com>
2021-01-05 22:48:08 +0300

    Initial Commit

---
+++ FunctionExample/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace FunctionExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            
+            ExampleOne();
+
+            ExampleTwo();
+        }
+        
+        private static void ExampleOne()
+        {
+            var k = 10;
+            
+            var j = 99.9;
+            
+            void Scope(string x, int y) 
+            {
+                Console.WriteLine($"The value of {nameof(x)} is {x}");
+                Console.WriteLine($"The value of {nameof(y)} is {y}");
+                Console.WriteLine($"The value of {nameof(k)} is {j}");
+                Console.WriteLine($"The value of {nameof(j)} is {j}");
+            }
+            
+            Scope("twelve", 666);
+            
+            Console.ReadKey();
+        }
+
+        private static void ExampleTwo()
+        {
+            
+            MyGeneric<string>("twelve");
+            
+            MyGeneric<int>(2012);
+            
+            void MyGeneric<Value>(Value x)
+            {
+                Console.WriteLine($"Value from generic message: {x}");
+            }
+            
+            Console.ReadKey();
+        }
+        
+        public void TotalCount(int numberOne, int numberTwo)
+        {
+            double Total()
+            {
+                return numberOne + numberTwo;
+            }
+            
+            Console.WriteLine($"Total: {Total()}");
+        }
+    }
+}
